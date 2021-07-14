2c8f6fe1029d8a022ceb1083917e57498e53742e
CNDvn <banhduchieucode@gmail.com>
2021-07-14 19:42:03 +0700

    Update Task status

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -26,4 +26,14 @@ export class TasksService {
   getTaskById(id: string): Task {
     return this.tasks.find((task) => task.id === id);
   }
+
+  deleteTask(id: string): void{
+    this.tasks = this.tasks.filter(task => task.id !== id);
+  }
+
+  updateTaskStatus(id:string, status:TaskStatus){
+    const task = this.getTaskById(id);
+    task.status = status;
+    return task;
+  }
 }
